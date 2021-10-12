705d5fabaec34b096a88c33aef2ab62d81d104b4
Sam Davis <hsndm16@yahoo.com>
2021-10-12 16:35:38 -0500

    final card flipping+animation(needs touching)

--- js/card.js
+++ js/card.js
@@ -1,16 +1,21 @@
 // <div class="card" style="width: 18rem;">
+//   <div>       
 //   <img class="card-img-top" src="..." alt="Card image cap">
 //   <div class="card-body">
 //     <h5 class="card-title">Card title</h5>
 //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 //     <a href="#" class="btn btn-primary">Go somewhere</a>
 //   </div>
+//   </div>
 // </div>
 
 //DOM CREATION
-let bodyElement = document.querySelector('body')
+let bodyElement = document.body
 
-let cardElement = document.createElement('div')
+let flipCard = document.createElement('div')
+let flipCardInner = document.createElement('div')
+let flipCardBack = document.createElement('div')
+let flipCardFront = document.createElement('div')
 let imageContainer = document.createElement('div')
 let infoContainer = document.createElement('div')
 let imageElement = document.createElement('img')
@@ -19,13 +24,16 @@ let pElement = document.createElement('p')
 let btnElement = document.createElement('button')
 
 //APPLYING CLASS NAMES TO DOM ELEMENTS/CONTAINERS
-cardElement.className = 'card'
+flipCardFront.className = 'flip-card-front card-face'
 imageContainer.className = 'image-container'
-infoContainer.className = 'info-container d-flex align-items-center justify-content-center'
+infoContainer.className = 'info-container'
 imageElement.className = 'image'
 headElement.className = 'heading'
 pElement.className = 'paragraph'
 btnElement.className = 'btn'
+flipCard.className = 'flip-card'
+flipCardInner.className = 'flip-card-inner'
+flipCardBack.className = 'flip-card-back card-face'
 
 //Applying source (api needed)
 imageElement.src = 'https://cdn.myanimelist.net/s/common/uploaded_files/1482965945-3d9561fa5a014da11e0dd3b2f148b1b0.jpeg' //`${anime[x].image_url}` 
@@ -36,20 +44,21 @@ btnElement.setAttribute('href', '#')
 headElement.innerText = '`${anime[x].name}`' //`${anime[x].name}`
 pElement.innerText = '`${quoteapi[x].quote}`' //`${quoteapi[x].quote}`
 btnElement.innerText = 'More info'
+flipCardBack.innerText = 'help'
+
+// card flipping/button functionality
+flipCardInner.addEventListener('click', ()=>{
+    flipCardInner.classList.toggle('is-flipped')
+})
 
 //APPENDING CARD TOGETHER
-bodyElement.appendChild(cardElement);
-cardElement.append(imageContainer, infoContainer);
+bodyElement.appendChild(flipCard); //
 
+flipCardFront.append(imageContainer, infoContainer);
 imageContainer.appendChild(imageElement)
 infoContainer.append(headElement, pElement, btnElement);
-
-
-
-
-
-
-
+flipCard.append(flipCardInner)
+flipCardInner.append(flipCardFront, flipCardBack)
 
 
 
