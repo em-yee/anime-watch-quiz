c0ad73ed31e04677e9cc1c88bdbe9b750ce0daa3
MercerMahaffey <89871123+MercerMahaffey@users.noreply.github.com>
2021-10-12 12:22:04 -0500

    testing 2nd api

--- js/firstApi.js
+++ js/firstApi.js
@@ -1,20 +1,38 @@
 let expression = "hello"
 
-    $(() => {
-
 function firstApi (genNum = 7) {
+    
+
+    
+
     
     genNum = genNum.toString();
     $.get(`https://api.jikan.moe/v3/genre/anime/${genNum}/1`).promise()
     .done(data => {
-        console.log(data);
-        // let dataArray = data['mal_url']['name'];
+        
+        let dataArray = data['anime'];
         // console.log(dataArray);
-        // return dataArray;
+        for (animeRecommendation of dataArray){
+            // console.log(animeRecommendation);
+            animeRecommendationTitle = animeRecommendation['title'];
+            // console.log(animeRecommendationTitle);
+            animeRecommendationImageURL = animeRecommendation['image_url'];
+            // console.log(animeRecommendationImageURL);
+            animeRecommendationSynopsis = animeRecommendation['synopsis'];
+            // console.log(animeRecommendationSynopsis);
+            
+            $.get(`https://animechan.vercel.app/api/quotes/anime?title=${animeRecommendationTitle.toLowerCase()}`)
+            .done(response => {
+                console.log(response);
+            })
+            
+            
+            }
     })
     
+    
+
     }
     
-    firstApi()
-    })
+
 
