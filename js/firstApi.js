17dce6ab3994778d57e9dd2495103133ff174e16
MercerMahaffey <89871123+MercerMahaffey@users.noreply.github.com>
2021-10-12 13:53:40 -0500

    testing 2nd api

--- js/firstApi.js
+++ js/firstApi.js
@@ -12,18 +12,19 @@ function firstApi (genNum = 7) {
         
         let dataArray = data['anime'];
         // console.log(dataArray);
-        for (animeRecommendation of dataArray){
-            // console.log(animeRecommendation);
-            animeRecommendationTitle = animeRecommendation['title'];
+        for (dataArrayIndex = 0; dataArrayIndex < 10; dataArrayIndex ++){
+            animeRecommendationTitle = dataArray[dataArrayIndex]['title'];
             // console.log(animeRecommendationTitle);
-            animeRecommendationImageURL = animeRecommendation['image_url'];
+            // console.log(typeof animeRecommendationTitle);
+            animeRecommendationImageURL = dataArray[dataArrayIndex]['image_url'];
             // console.log(animeRecommendationImageURL);
-            animeRecommendationSynopsis = animeRecommendation['synopsis'];
+            animeRecommendationSynopsis = dataArray[dataArrayIndex]['synopsis'];
             // console.log(animeRecommendationSynopsis);
             
             $.get(`https://animechan.vercel.app/api/quotes/anime?title=${animeRecommendationTitle.toLowerCase()}`)
             .done(response => {
-                console.log(response);
+                // console.log(response);
+                console.log(animeRecommendationTitle);
             })
             
             
