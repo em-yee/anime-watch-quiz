d84254f388502992feddee2d00ba906c41c159a0
MercerMahaffey <89871123+MercerMahaffey@users.noreply.github.com>
2021-10-13 09:20:41 -0500

    first merge

--- js/firstApi.js
+++ js/firstApi.js
@@ -1,39 +1,55 @@
-let expression = "hello"
 
-function firstApi (genNum = 7) {
+function firstApi (genreNumber = 7) {
+    let animeRecommendationObjectList = [];
     
+    genreNumber = genreNumber.toString();
 
-    
-
-    
-    genNum = genNum.toString();
-    $.get(`https://api.jikan.moe/v3/genre/anime/${genNum}/1`).promise()
+    $.get(`https://api.jikan.moe/v3/genre/anime/${genreNumber}/1`)
     .done(data => {
         
         let dataArray = data['anime'];
         // console.log(dataArray);
-        for (dataArrayIndex = 0; dataArrayIndex < 10; dataArrayIndex ++){
-            animeRecommendationTitle = dataArray[dataArrayIndex]['title'];
+        for (dataArrayIndex = 0; dataArrayIndex < 3; dataArrayIndex ++){
+            let animeRecommendationTitle = dataArray[dataArrayIndex]['title'];
             // console.log(animeRecommendationTitle);
             // console.log(typeof animeRecommendationTitle);
-            animeRecommendationImageURL = dataArray[dataArrayIndex]['image_url'];
+            let animeRecommendationImageURL = dataArray[dataArrayIndex]['image_url'];
             // console.log(animeRecommendationImageURL);
-            animeRecommendationSynopsis = dataArray[dataArrayIndex]['synopsis'];
+            let animeRecommendationSynopsis = dataArray[dataArrayIndex]['synopsis'];
             // console.log(animeRecommendationSynopsis);
             
-            $.get(`https://animechan.vercel.app/api/quotes/anime?title=${animeRecommendationTitle.toLowerCase()}`)
-            .done(response => {
-                // console.log(response);
-                console.log(animeRecommendationTitle);
-            })
+
+            let animeRecommendationQuote = "";
             
+            // setting animeSearches object to the local storage if it exists
+            let animeSearches = localStorage['animeSearches'] || "{}";
+            console.log(animeSearches);
+            animeSearches = JSON.parse(animeSearches);
+            
+            // if local storage does not hold the quote
+            if (!animeSearches[animeRecommendationTitle]) {
+                $.get(`https://animechan.vercel.app/api/quotes/anime?title=${animeRecommendationTitle.toLowerCase()}`)
+                .done(response => {
+                    console.log("searching api");
+                    animeSearches[animeRecommendationTitle] = response[0]['quote'];
+                    localStorage['animeSearches'] = JSON.stringify(animeSearches);
+                    animeRecommendationQuote = animeSearches[animeRecommendationTitle];
+                    
+                    let animeRecommendationObject = {title: animeRecommendationTitle, image: animeRecommendationImageURL, synopsis: animeRecommendationSynopsis, quote: animeRecommendationQuote};
+                    animeRecommendationObjectList.push(animeRecommendationObject);
+                    
+                })
+            }// if local storage does hold the quote
+            else{
+                console.log("using local storage");
+                animeRecommendationQuote = animeSearches[animeRecommendationTitle];
+                let animeRecommendationObject = {title: animeRecommendationTitle, image: animeRecommendationImageURL, synopsis: animeRecommendationSynopsis, quote: animeRecommendationQuote}
+                animeRecommendationObjectList.push(animeRecommendationObject)
+            }
             
             }
     })
     
-    
+    return animeRecommendationObjectList
 
     }
-    
-
-
